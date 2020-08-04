eba5f6a485eb33fde690414957fb4cfc4fcd207f
windtagane <windtagane@gmail.com>
2020-08-04 10:19:50 +0400

    fix migration create_roles_table

--- prudencecreole/database/migrations/2020_08_03_094458_create_roles_table.php
+++ prudencecreole/database/migrations/2020_08_03_094458_create_roles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableRole extends Migration
+class CreateRolesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,6 +14,10 @@ class CreateTableRole extends Migration
     public function up()
     {
         //
+         Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('role');
+        });
     }
 
     /**
@@ -23,9 +27,6 @@ class CreateTableRole extends Migration
      */
     public function down()
     {
-        Schema::create('roles', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('role');
-        });
+        Schema::dropIfExists('roles');
     }
 }
